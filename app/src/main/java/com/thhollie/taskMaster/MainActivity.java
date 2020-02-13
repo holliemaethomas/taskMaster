a010dbbcc66fa28ef817d414e7ef9ff13157fe01
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-13 11:21:06 -0800

    added functional return button

--- app/src/main/java/com/thhollie/taskMaster/MainActivity.java
+++ app/src/main/java/com/thhollie/taskMaster/MainActivity.java
@@ -2,13 +2,29 @@ package com.thhollie.taskMaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "ThHollie.main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button addTask = findViewById(R.id.goToAddTaskBtn);
+        addTask.setOnClickListener((v) -> {
+            Intent addToTaskList = new Intent(MainActivity.this, addTask.class);
+            MainActivity.this.startActivity(addToTaskList);
+        });
+
+        Button viewTaskList = findViewById(R.id.viewTasks);
+        viewTaskList.setOnClickListener((v) -> {
+            Intent showTaskList = new Intent(MainActivity.this, viewTasks.class);
+            MainActivity.this.startActivity(showTaskList);
+        });
     }
 }
--- app/src/main/java/com/thhollie/taskMaster/addTask.java
+++ app/src/main/java/com/thhollie/taskMaster/addTask.java
@@ -2,6 +2,7 @@ package com.thhollie.taskMaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +16,13 @@ public class addTask extends AppCompatActivity {
         Button addedTaskBtn = findViewById(R.id.addTaskBtn);
     }
     protected void returnToPrevious(View v) {
-        View returnToPrevious = findViewById(R.id.addTaskBtn);
+        View returnToPrevious = findViewById(R.id.returnToHomeBtn);
         this.finish();
     }
     public void showMessage(View v){
         Toast confirmToast = Toast.makeText(this, R.string.confirmed,Toast.LENGTH_LONG);
         confirmToast.show();
     }
+
+
 }
