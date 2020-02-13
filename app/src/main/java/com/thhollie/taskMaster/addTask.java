bd4dc0589876f3f5dc9f0d2241fec85d69627727
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-13 10:01:17 -0800

    added file content back in

---
+++ app/src/main/java/com/thhollie/taskMaster/addTask.java
@@ -0,0 +1,25 @@
+package com.thhollie.taskMaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class addTask extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        Button addedTaskBtn = findViewById(R.id.addTaskBtn);
+    }
+    protected void returnToPrevious(View v) {
+        View returnToPrevious = findViewById(R.id.addTaskBtn);
+        this.finish();
+    }
+    public void showMessage(View v){
+        Toast confirmToast = Toast.makeText(this, R.string.confirmed,Toast.LENGTH_LONG);
+        confirmToast.show();
+    }
+}
---
+++ app/src/main/java/com/thhollie/taskMaster/view_tasks.java
@@ -0,0 +1,12 @@
+package com.thhollie.taskMaster;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class view_tasks extends AppCompatActivity {
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_tasks);
+    }
+}
